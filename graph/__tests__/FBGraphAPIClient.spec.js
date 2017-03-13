20fd0d01f3e63443e9203bf74a1d0db32dbef828
C.T. Lin <chentsulin@gmail.com>
2017-03-13 17:28:06 +0800

    added tests for send api

--- graph/__tests__/FBGraphAPIClient.spec.js
+++ graph/__tests__/FBGraphAPIClient.spec.js
@@ -2,6 +2,7 @@ import MockAdapter from 'axios-mock-adapter';
 
 import FBGraphAPIClient from '../FBGraphAPIClient';
 
+const RECIPIENT_ID = '1QAZ2WSX';
 const ACCESS_TOKEN = '1234567890';
 
 const createMock = () => {
@@ -411,6 +412,532 @@ describe('domain whitelist', () => {
         .reply(200, expected);
 
       const res = await client.deleteDomainWhitelist();
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+});
+
+describe('sned api', () => {
+  describe('#send', () => {
+    it('should call messages api', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            text: 'Hello!',
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.send(RECIPIENT_ID, {
+        text: 'Hello!',
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendAttachment', () => {
+    it('should call messages api with attachment', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'image',
+              payload: {
+                url: 'https://example.com/pic.png',
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendAttachment(RECIPIENT_ID, {
+        type: 'image',
+        payload: {
+          url: 'https://example.com/pic.png',
+        },
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendText', () => {
+    it('should call messages api with text', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            text: 'Hello!',
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendText(RECIPIENT_ID, 'Hello!');
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendAudio', () => {
+    it('should call messages api with audio', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'audio',
+              payload: {
+                url: 'https://example.com/audio.mp3',
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendAudio(
+        RECIPIENT_ID,
+        'https://example.com/audio.mp3',
+      );
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendImage', () => {
+    it('should call messages api with audio', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'image',
+              payload: {
+                url: 'https://example.com/pic.png',
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendImage(
+        RECIPIENT_ID,
+        'https://example.com/pic.png',
+      );
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendVideo', () => {
+    it('should call messages api with video', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'video',
+              payload: {
+                url: 'https://example.com/video.mp4',
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendVideo(
+        RECIPIENT_ID,
+        'https://example.com/video.mp4',
+      );
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendFile', () => {
+    it('should call messages api with file', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'file',
+              payload: {
+                url: 'https://example.com/word.docx',
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendFile(
+        RECIPIENT_ID,
+        'https://example.com/word.docx',
+      );
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendTemplate', () => {
+    it('should call messages api with template', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'template',
+              payload: {
+                template_type: 'button',
+                text: 'title',
+                buttons: [
+                  {
+                    type: 'postback',
+                    title: 'Start Chatting',
+                    payload: 'USER_DEFINED_PAYLOAD',
+                  },
+                ],
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendTemplate(RECIPIENT_ID, {
+        template_type: 'button',
+        text: 'title',
+        buttons: [
+          {
+            type: 'postback',
+            title: 'Start Chatting',
+            payload: 'USER_DEFINED_PAYLOAD',
+          },
+        ],
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendButtonTemplate', () => {
+    it('should call messages api with button template', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'template',
+              payload: {
+                template_type: 'button',
+                text: 'title',
+                buttons: [
+                  {
+                    type: 'postback',
+                    title: 'Start Chatting',
+                    payload: 'USER_DEFINED_PAYLOAD',
+                  },
+                ],
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendButtonTemplate(RECIPIENT_ID, 'title', [
+        {
+          type: 'postback',
+          title: 'Start Chatting',
+          payload: 'USER_DEFINED_PAYLOAD',
+        },
+      ]);
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendGenericTemplate', () => {
+    it('should call messages api with generic template', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            attachment: {
+              type: 'template',
+              payload: {
+                template_type: 'generic',
+                elements: [
+                  {
+                    title: "Welcome to Peter's Hats",
+                    image_url: 'https://petersfancybrownhats.com/company_image.png',
+                    subtitle: "We've got the right hat for everyone.",
+                    default_action: {
+                      type: 'web_url',
+                      url: 'https://peterssendreceiveapp.ngrok.io/view?item=103',
+                      messenger_extensions: true,
+                      webview_height_ratio: 'tall',
+                      fallback_url: 'https://peterssendreceiveapp.ngrok.io/',
+                    },
+                    buttons: [
+                      {
+                        type: 'postback',
+                        title: 'Start Chatting',
+                        payload: 'DEVELOPER_DEFINED_PAYLOAD',
+                      },
+                    ],
+                  },
+                ],
+                image_aspect_ratio: 'square',
+              },
+            },
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendGenericTemplate(RECIPIENT_ID, [
+        {
+          title: "Welcome to Peter's Hats",
+          image_url: 'https://petersfancybrownhats.com/company_image.png',
+          subtitle: "We've got the right hat for everyone.",
+          default_action: {
+            type: 'web_url',
+            url: 'https://peterssendreceiveapp.ngrok.io/view?item=103',
+            messenger_extensions: true,
+            webview_height_ratio: 'tall',
+            fallback_url: 'https://peterssendreceiveapp.ngrok.io/',
+          },
+          buttons: [
+            {
+              type: 'postback',
+              title: 'Start Chatting',
+              payload: 'DEVELOPER_DEFINED_PAYLOAD',
+            },
+          ],
+        },
+      ]);
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#sendQuickReplies', () => {
+    it('should call messages api with quick replies', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          message: {
+            text: 'Pick a color:',
+            attachment: null,
+            quick_replies: [
+              {
+                content_type: 'text',
+                title: 'Red',
+                payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED',
+              },
+            ],
+          },
+        })
+        .reply(200, expected);
+
+      const res = await client.sendQuickReplies(
+        RECIPIENT_ID,
+        'Pick a color:',
+        null,
+        [
+          {
+            content_type: 'text',
+            title: 'Red',
+            payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED',
+          },
+        ],
+      );
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#setSenderAction', () => {
+    it('should call messages api with sender action', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          sender_action: 'typing_on',
+        })
+        .reply(200, expected);
+
+      const res = await client.setSenderAction(RECIPIENT_ID, 'typing_on');
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#turnTypingIndicatorsOn', () => {
+    it('should call messages api with typing_on sender action', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          sender_action: 'typing_on',
+        })
+        .reply(200, expected);
+
+      const res = await client.turnTypingIndicatorsOn(RECIPIENT_ID);
+
+      expect(res.status).toBe(200);
+      expect(res.data).toBe(expected);
+    });
+  });
+
+  describe('#turnTypingIndicatorsOff', () => {
+    it('should call messages api with typing_off sender action', async () => {
+      const { client, mock } = createMock();
+
+      const expected = {
+        recipient_id: RECIPIENT_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      mock
+        .onPost(`/me/messages?access_token=${ACCESS_TOKEN}`, {
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+          sender_action: 'typing_off',
+        })
+        .reply(200, expected);
+
+      const res = await client.turnTypingIndicatorsOff(RECIPIENT_ID);
 
       expect(res.status).toBe(200);
       expect(res.data).toBe(expected);
