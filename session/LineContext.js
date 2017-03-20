b2fc0ed17fe7a287abdfd2b1ee9d7a7551f3c878
C.T. Lin <chentsulin@gmail.com>
2017-03-20 18:43:04 +0800

    add methods to context

--- session/LineContext.js
+++ session/LineContext.js
@@ -36,4 +36,64 @@ export default class LineContext extends Context {
       delay: this._getMessageDelay(text),
     });
   }
+
+  sendImage(contentUrl: string, previewUrl: ?string): void {
+    this._jobQueue.enqueue({
+      instance: this._client,
+      method: 'pushImage',
+      args: [this._data.user.id, contentUrl, previewUrl],
+      delay: this._getMessageDelay(),
+    });
+  }
+
+  sendVideo(contentUrl: string, previewUrl: string): void {
+    this._jobQueue.enqueue({
+      instance: this._client,
+      method: 'pushVideo',
+      args: [this._data.user.id, contentUrl, previewUrl],
+      delay: this._getMessageDelay(),
+    });
+  }
+
+  sendAudio(contentUrl: string, duration: number): void {
+    this._jobQueue.enqueue({
+      instance: this._client,
+      method: 'pushAudio',
+      args: [this._data.user.id, contentUrl, duration],
+      delay: this._getMessageDelay(),
+    });
+  }
+
+  sendLocation(
+    {
+      title,
+      address,
+      latitude,
+      longitude,
+    },
+  ): void {
+    this._jobQueue.enqueue({
+      instance: this._client,
+      method: 'pushLocation',
+      args: [
+        this._data.user.id,
+        {
+          title,
+          address,
+          latitude,
+          longitude,
+        },
+      ],
+      delay: this._getMessageDelay(),
+    });
+  }
+
+  sendSticker(packageId: string, stickerId: string): void {
+    this._jobQueue.enqueue({
+      instance: this._client,
+      method: 'pushSticker',
+      args: [this._data.user.id, packageId, stickerId],
+      delay: this._getMessageDelay(),
+    });
+  }
 }
