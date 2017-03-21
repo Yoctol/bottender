a4d41bcbd98eafe1ec088bf44bff1d461689e8c1
C.T. Lin <chentsulin@gmail.com>
2017-03-21 21:22:48 +0800

    Added tests for LineContext

--- session/__tests__/LineContext.spec.js
+++ session/__tests__/LineContext.spec.js
@@ -150,6 +150,184 @@ it('put pushSticker to jobQueue', () => {
   });
 });
 
+it('put pushImagemap to jobQueue', () => {
+  const { context, client, data } = setup();
+  context._jobQueue = {
+    enqueue: jest.fn(),
+  };
+
+  const template = {
+    baseUrl: 'https://example.com/bot/images/rm001',
+    baseHeight: 1040,
+    baseWidth: 1040,
+    actions: [
+      {
+        type: 'uri',
+        linkUri: 'https://example.com/',
+        area: {
+          x: 0,
+          y: 0,
+          width: 520,
+          height: 1040,
+        },
+      },
+      {
+        type: 'message',
+        text: 'hello',
+        area: {
+          x: 520,
+          y: 0,
+          width: 520,
+          height: 1040,
+        },
+      },
+    ],
+  };
+
+  context.sendImagemap('this is an imagemap', template);
+
+  expect(context._jobQueue.enqueue).toBeCalledWith({
+    instance: client,
+    method: 'pushImagemap',
+    args: [data.user.id, 'this is an imagemap', template],
+    delay: 1000,
+  });
+});
+
+it('put pushButtonTemplate to jobQueue', () => {
+  const { context, client, data } = setup();
+  context._jobQueue = {
+    enqueue: jest.fn(),
+  };
+
+  const template = {
+    thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+    title: 'Menu',
+    text: 'Please select',
+    actions: [
+      {
+        type: 'postback',
+        label: 'Buy',
+        data: 'action=buy&itemid=123',
+      },
+      {
+        type: 'postback',
+        label: 'Add to cart',
+        data: 'action=add&itemid=123',
+      },
+      {
+        type: 'uri',
+        label: 'View detail',
+        uri: 'http://example.com/page/123',
+      },
+    ],
+  };
+
+  context.sendButtonTemplate('this is a button template', template);
+
+  expect(context._jobQueue.enqueue).toBeCalledWith({
+    instance: client,
+    method: 'pushButtonTemplate',
+    args: [data.user.id, 'this is a button template', template],
+    delay: 1000,
+  });
+});
+
+it('put pushConfirmTemplate to jobQueue', () => {
+  const { context, client, data } = setup();
+  context._jobQueue = {
+    enqueue: jest.fn(),
+  };
+
+  const template = {
+    text: 'Are you sure?',
+    actions: [
+      {
+        type: 'message',
+        label: 'Yes',
+        text: 'yes',
+      },
+      {
+        type: 'message',
+        label: 'No',
+        text: 'no',
+      },
+    ],
+  };
+
+  context.sendConfirmTemplate('this is a confirm template', template);
+
+  expect(context._jobQueue.enqueue).toBeCalledWith({
+    instance: client,
+    method: 'pushConfirmTemplate',
+    args: [data.user.id, 'this is a confirm template', template],
+    delay: 1000,
+  });
+});
+
+it('put pushCarouselTemplate to jobQueue', () => {
+  const { context, client, data } = setup();
+  context._jobQueue = {
+    enqueue: jest.fn(),
+  };
+
+  const template = [
+    {
+      thumbnailImageUrl: 'https://example.com/bot/images/item1.jpg',
+      title: 'this is menu',
+      text: 'description',
+      actions: [
+        {
+          type: 'postback',
+          label: 'Buy',
+          data: 'action=buy&itemid=111',
+        },
+        {
+          type: 'postback',
+          label: 'Add to cart',
+          data: 'action=add&itemid=111',
+        },
+        {
+          type: 'uri',
+          label: 'View detail',
+          uri: 'http://example.com/page/111',
+        },
+      ],
+    },
+    {
+      thumbnailImageUrl: 'https://example.com/bot/images/item2.jpg',
+      title: 'this is menu',
+      text: 'description',
+      actions: [
+        {
+          type: 'postback',
+          label: 'Buy',
+          data: 'action=buy&itemid=222',
+        },
+        {
+          type: 'postback',
+          label: 'Add to cart',
+          data: 'action=add&itemid=222',
+        },
+        {
+          type: 'uri',
+          label: 'View detail',
+          uri: 'http://example.com/page/222',
+        },
+      ],
+    },
+  ];
+
+  context.sendCarouselTemplate('this is a carousel template', template);
+
+  expect(context._jobQueue.enqueue).toBeCalledWith({
+    instance: client,
+    method: 'pushCarouselTemplate',
+    args: [data.user.id, 'this is a carousel template', template],
+    delay: 1000,
+  });
+});
+
 it('use default message delay when nothing passed in', () => {
   const { context, client, data } = setup(null, true);
 
