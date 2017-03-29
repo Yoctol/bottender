6585d2a7ffcb894a896a3c237f2e0c6954b2996f
C.T. Lin <chentsulin@gmail.com>
2017-03-29 14:54:04 +0800

    deprecated logMessage

--- middlewares/logMessage.js
+++ middlewares/logMessage.js
@@ -1,9 +1,14 @@
-const logMessage = ({ db }) =>
-  async ({ request }, next) => {
+import warning from 'warning';
+
+const logMessage = ({ db }) => {
+  warning(false, '`logMessage` is deprecated.');
+
+  return async ({ request }, next) => {
     const msg = request.body.entry[0].messaging[0];
     const message = JSON.stringify(msg);
     await db.insert({ text: message, alien_id: null }).into('messages');
     return next();
   };
+};
 
 export default logMessage;
