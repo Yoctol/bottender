44d8a35287fe9e38e12594df4715a987f9e71391
C.T. Lin <chentsulin@gmail.com>
2017-04-14 19:30:15 +0800

    MongoSessionStore

---
+++ session/MongoSessionStore.js
@@ -0,0 +1,32 @@
+import { resolveScoped } from '../database/resolve';
+
+export default class MongoSessionStore {
+  constructor({ id }) {
+    this._id = id;
+  }
+
+  async init() {
+    this._db = await resolveScoped(this._id);
+    return this;
+  }
+
+  async get(key) {
+    const [platform, id] = key.split(':');
+    return this._sessions.findOne({ platform, id });
+  }
+
+  async set(key, sess /* , maxAge */) {
+    const [platform, id] = key.split(':');
+    const filter = { platform, id };
+    return this._sessions.replaceOne(filter, sess, { upsert: true });
+  }
+
+  async destroy(key) {
+    const [platform, id] = key.split(':');
+    return this._sessions.remove({ platform, id });
+  }
+
+  get _sessions() {
+    return this._db.collection('sessions');
+  }
+}
