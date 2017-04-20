df2bc28e70839301f693d59e7fd43ab6e462703d
C.T. Lin <chentsulin@gmail.com>
2017-04-20 18:49:49 +0800

    update core api for default template

--- bot/LINEBot.js
+++ bot/LINEBot.js
@@ -2,8 +2,8 @@ import Bot from './Bot';
 import LINEConnector from './LINEConnector';
 
 export default class LINEBot extends Bot {
-  constructor({ id, accessToken, channelSecret, filePath }) {
+  constructor({ id, accessToken, channelSecret }) {
     const connector = new LINEConnector({ accessToken, channelSecret });
-    super({ id, filePath, connector });
+    super({ id, connector });
   }
 }
--- bot/MessengerBot.js
+++ bot/MessengerBot.js
@@ -2,8 +2,8 @@ import Bot from './Bot';
 import MessengerConnector from './MessengerConnector';
 
 export default class MessengerBot extends Bot {
-  constructor({ id, accessToken, filePath }) {
+  constructor({ id, accessToken }) {
     const connector = new MessengerConnector(accessToken);
-    super({ id, filePath, connector });
+    super({ id, connector });
   }
 }
