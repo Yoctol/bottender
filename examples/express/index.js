b1837256c0acfeeceea8c6bfd6a4daff728ba2eb
C.T. Lin <chentsulin@gmail.com>
2017-06-02 11:41:41 +0800

    finish express example

--- examples/express/index.js
+++ examples/express/index.js
@@ -1,27 +1,41 @@
+require('babel-register');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { MessengerBot } = require('../../src');
+const {
+  MessengerBot,
+  MemoryCacheStore,
+  CacheBasedSessionStore,
+} = require('../../src');
 const { verifyMessengerWebhook } = require('../../src/express');
 
 const config = {
-  verifyToken: '',
-  accessToken: '',
+  verifyToken: '1qaz2wsx',
+  accessToken: '__FILL_YOUR_TOKEN_HERE__',
 };
+
+const cache = new MemoryCacheStore(500);
+const sessionHandler = new CacheBasedSessionStore(cache);
 
 const bot = new MessengerBot({
   accessToken: config.accessToken,
+  sessionHandler,
+});
+
+bot.handle(context => {
+  context.sendText('Hello World');
 });
 
 const server = express();
 
 server.use(bodyParser.json());
 server.get(
+  '/',
   verifyMessengerWebhook({
     verifyToken: config.verifyToken,
   })
 );
-server.post(bot.createExpressMiddleware());
+server.post('/', bot.createExpressMiddleware());
 
 server.listen(5000, () => {
   console.log('server is running...');
