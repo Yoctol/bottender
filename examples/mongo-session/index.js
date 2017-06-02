d6b28295ac33a40d3e15dc07e38cde5eb57265f8
C.T. Lin <chentsulin@gmail.com>
2017-06-02 14:17:44 +0800

    mongo session example

---
+++ examples/mongo-session/index.js
@@ -0,0 +1,38 @@
+require('babel-register');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { MessengerBot, MongoSessionStore } = require('../../src');
+const { verifyMessengerWebhook } = require('../../src/express');
+
+const config = {
+  verifyToken: '1qaz2wsx',
+  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+};
+
+const MONGO_URL = 'mongodb://localhost:27017/';
+const sessionHandler = new MongoSessionStore(MONGO_URL);
+
+const bot = new MessengerBot({
+  accessToken: config.accessToken,
+  sessionHandler,
+});
+
+bot.handle(context => {
+  context.sendText('Hello World');
+});
+
+const server = express();
+
+server.use(bodyParser.json());
+server.get(
+  '/',
+  verifyMessengerWebhook({
+    verifyToken: config.verifyToken,
+  })
+);
+server.post('/', bot.createExpressMiddleware());
+
+server.listen(5000, () => {
+  console.log('server is running...');
+});
