c871d78303cc24412ff96eac73326598ea910f8e
C.T. Lin <chentsulin@gmail.com>
2017-06-15 17:10:54 +0800

    Added types for LINEBot, MessengerBot

--- src/bot/LINEBot.js
+++ src/bot/LINEBot.js
@@ -1,8 +1,20 @@
+/* @flow */
+
+import type { SessionStore } from '../session/SessionStore';
+
 import Bot from './Bot';
 import LINEConnector from './LINEConnector';
 
 export default class LINEBot extends Bot {
-  constructor({ accessToken, channelSecret, sessionStore }) {
+  constructor({
+    accessToken,
+    channelSecret,
+    sessionStore,
+  }: {
+    accessToken: string,
+    channelSecret: string,
+    sessionStore: SessionStore,
+  }) {
     const connector = new LINEConnector({ accessToken, channelSecret });
     super({ connector, sessionStore });
   }
