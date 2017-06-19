440e739d4e5f09429040c5a470eb07140a2cf622
C.T. Lin <chentsulin@gmail.com>
2017-06-19 18:08:59 +0800

    Added LINE hello world example

---
+++ examples/line-hello-world/index.js
@@ -0,0 +1,24 @@
+require('babel-register');
+
+const { LINEBot } = require('../../src');
+const { createServer } = require('../../src/express');
+
+const config = {
+  channelSecret: '__FILL_YOUR_SECRET_HERE__',
+  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+};
+
+const bot = new LINEBot({
+  channelSecret: config.channelSecret,
+  accessToken: config.accessToken,
+});
+
+bot.handle(context => {
+  context.sendText('Hello World');
+});
+
+const server = createServer(bot, config);
+
+server.listen(5000, () => {
+  console.log('server is running...');
+});
