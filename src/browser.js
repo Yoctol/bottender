193c7aa275dcc7d18652c9eb96b4f4315058196b
C.T. Lin <chentsulin@gmail.com>
2017-07-15 16:47:04 +0800

    add browser field to package.json

---
+++ src/browser.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+/* Bot */
+export { default as Bot } from './bot/Bot';
+
+/* Connector */
+export { default as Connector } from './bot/Connector';
+
+/* HandlerBuilder */
+export { default as BasicHandlerBuilder } from './bot/BasicHandlerBuilder';
+export {
+  default as ClassifierHandlerBuilder,
+} from './bot/ClassifierHandlerBuilder';
+export {
+  default as MiddlewareHandlerBuilder,
+} from './bot/MiddlewareHandlerBuilder';
+
+/* Cache */
+export { default as MemoryCacheStore } from './cache/MemoryCacheStore';
+
+/* Session */
+export {
+  default as CacheBasedSessionStore,
+} from './session/CacheBasedSessionStore';
