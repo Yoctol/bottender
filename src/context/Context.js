4f63f7486b39f1a3704a38f92f86443096359511
C.T. Lin <chentsulin@gmail.com>
2017-08-09 00:12:42 +0800

    Added platform to Context interface

--- src/context/Context.js
+++ src/context/Context.js
@@ -5,6 +5,7 @@ import type { Event } from './Event';
 export const DEFAULT_MESSAGE_DELAY = 1000;
 
 export interface Context {
+  +platform: string,
   +event: Event,
   +session: {},
   sendText?: (text: string) => void,
