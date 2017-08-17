f63c7ddfdb82db88be4d5e75a2f05f637427143a
tw0517tw <tw0517tw@gmail.com>
2017-08-17 17:41:18 +0800

    add verifyLINESignature to restify

--- src/restify/createMiddleware.js
+++ src/restify/createMiddleware.js
@@ -1,12 +1,13 @@
 function createMiddleware(bot) {
   const requestHandler = bot.createRequestHandler();
   return async (req, res, next) => {
-    if (!req.params) {
+    if (!req.body) {
       throw new Error(
         'createMiddleware(): Missing body parser. Use `restify.plugins.bodyParser()` before this middleware.'
       );
     }
-    await requestHandler(req.params);
+
+    await requestHandler(req.body);
     res.send(200);
     return next();
   };
