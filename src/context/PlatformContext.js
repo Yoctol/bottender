d7db2163b9c5cd3f3104cff5fd93c3ec9ef831a7
C.T. Lin <chentsulin@gmail.com>
2017-09-28 14:08:17 +0800

    upgrade dependencies

--- src/context/PlatformContext.js
+++ src/context/PlatformContext.js
@@ -3,8 +3,8 @@
 import type { Event } from './Event';
 
 export interface PlatformContext {
-  +platform: string,
-  +event: Event,
-  +session: ?{},
-  +sendText: (text: string) => any,
+  +platform: string;
+  +event: Event;
+  +session: ?{};
+  +sendText: (text: string) => any;
 }
