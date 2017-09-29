b73f6ecf05c087e1d798964c97bab2f7fac978b9
C.T. Lin <chentsulin@gmail.com>
2017-09-29 12:10:24 +0800

    init cross-platform example

---
+++ examples/cross-platform/index.js
@@ -0,0 +1,22 @@
+require('babel-register');
+
+const { MessengerBot } = require('../../src');
+const { createServer } = require('../../src/express');
+
+const config = {
+  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+};
+
+const bot = new MessengerBot({
+  accessToken: config.accessToken,
+});
+
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+const server = createServer(bot);
+
+server.listen(5000, () => {
+  console.log('server is running on 5000 port...');
+});
