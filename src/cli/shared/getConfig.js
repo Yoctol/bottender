0131b1b9036db0928a1b22890da3573957a55005
kpman <s92f002@hotmail.com>
2017-09-30 16:06:10 +0800

    Change getConfig policy

--- src/cli/shared/getConfig.js
+++ src/cli/shared/getConfig.js
@@ -1,15 +1,17 @@
 import path from 'path';
 
+import invariant from 'invariant';
 import get from 'lodash/get';
 import importFresh from 'import-fresh';
 
 const getConfig = (configPath, platform) => {
-  try {
-    const config = importFresh(path.resolve(configPath));
-    return get(config, platform, config);
-  } catch (e) {
-    console.error('No module required.');
-  }
+  const config = importFresh(path.resolve(configPath));
+  const result = get(config, platform, undefined);
+  invariant(
+    result,
+    `Could not find \`${platform}\` key, please check your config file is in the correct format.`
+  );
+  return result;
 };
 
 module.exports = getConfig;
