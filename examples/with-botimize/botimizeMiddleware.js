e511e149a105afa8f7dbf05b609fa525f92d6b1b
吳東曄 Wu, Dung-Ie <tw0517tw@gmail.com>
2017-10-19 17:17:42 +0800

    check for more platform in botmize middleware (#531)

--- examples/with-botimize/botimizeMiddleware.js
+++ examples/with-botimize/botimizeMiddleware.js
@@ -10,12 +10,13 @@ module.exports = function botimizeMiddleware(bot, { apiKey, platform }) {
   // Add a response interceptor
   axios.interceptors.response.use(response => {
     const { config } = response;
-    if (config.url.indexOf('/me/messages') !== -1) {
+    if (
+      /graph\.facebook\.com.*\/me\/messages/.test(config.url) || // Messenger
+      /api\.line\.me\/v2\/bot\/message/.test(config.url) || // LINE
+      /api\.telegram\.org.*\/send/.test(config.url) // Telegram
+    ) {
       botimize.logOutgoing(
-        {
-          ...JSON.parse(config.data),
-          accessToken,
-        },
+        { ...JSON.parse(config.data), accessToken },
         { parse: 'pure' }
       );
     }
