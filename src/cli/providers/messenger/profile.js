1df5c232bcb97ececf0055b2f467a32cf1f61544
kpman <s92f002@hotmail.com>
2017-11-23 13:04:51 +0800

    Let user can force update messenger profile by CLI

--- src/cli/providers/messenger/profile.js
+++ src/cli/providers/messenger/profile.js
@@ -1,9 +1,10 @@
 /* eslint-disable consistent-return */
 import { MessengerClient } from 'messaging-api-messenger';
 import invariant from 'invariant';
+import { diff, addedDiff, deletedDiff, updatedDiff } from 'deep-object-diff';
 
 import getConfig from '../../shared/getConfig';
-import { print, error, bold } from '../../shared/log';
+import { print, error, bold, log } from '../../shared/log';
 
 import help from './help';
 
@@ -17,6 +18,16 @@ const FIELDS = [
   'target_audience',
   'home_url',
 ];
+
+export const trimDomain = profile => {
+  const clone = Object.assign({}, profile);
+  if (clone.whitelisted_domains) {
+    clone.whitelisted_domains = clone.whitelisted_domains.map(domain =>
+      domain.replace(/\/$/, '')
+    );
+  }
+  return clone;
+};
 
 export async function getMessengerProfile() {
   try {
@@ -49,9 +60,10 @@ export async function getMessengerProfile() {
   }
 }
 
-export async function setMessengerProfile() {
+export async function setMessengerProfile(ctx) {
+  const { force } = ctx.argv;
   try {
-    const { accessToken, profile } = getConfig(
+    const { accessToken, profile: _profile } = getConfig(
       'bottender.config.js',
       'messenger'
     );
@@ -60,9 +72,49 @@ export async function setMessengerProfile() {
 
     const client = MessengerClient.connect(accessToken);
 
-    await client.setMessengerProfile(profile);
+    if (force) {
+      await client.deleteMessengerProfile(FIELDS);
+      print(
+        `Successfully delete all ${bold(
+          'messenger_profile'
+        )} settings due to ${bold('--force')} option`
+      );
 
-    print(`Successfully set ${bold('messenger_profile')} settings`);
+      await client.setMessengerProfile(_profile);
+      print(`Successfully set ${bold('messenger_profile')} settings`);
+    } else {
+      const [_existedProfile] = await client.getMessengerProfile(FIELDS);
+
+      const profile = trimDomain(_profile);
+      const existedProfile = trimDomain(_existedProfile);
+
+      const diffResult = diff(existedProfile, profile);
+      if (Object.keys(diffResult).length !== 0) {
+        const shouldDeleteProfile = deletedDiff(existedProfile, profile);
+        const shouldSetProfile = {
+          ...addedDiff(existedProfile, profile),
+          ...updatedDiff(existedProfile, profile),
+        };
+        if (Object.keys(shouldDeleteProfile).length > 0) {
+          await client.deleteMessengerProfile(Object.keys(shouldDeleteProfile));
+          const deleteFileds = Object.keys(shouldDeleteProfile).join(', ');
+          print(`Successfully delete ${bold(deleteFileds)} settings`);
+        }
+        if (Object.keys(shouldSetProfile).length > 0) {
+          await client.setMessengerProfile(shouldSetProfile);
+          const setFields = Object.keys(shouldSetProfile).join(', ');
+          print(`Successfully set ${bold(setFields)} settings`);
+        }
+      } else {
+        print(`No change apply because the profile settings is the same.`);
+      }
+    }
+
+    log(
+      `You can use ${bold(
+        'bottender messenger profile get'
+      )} to see the full profile setting.`
+    );
   } catch (err) {
     error(`Failed to set ${bold('messenger_profile')} settings`);
     if (err.response) {
@@ -109,7 +161,7 @@ export default async function main(ctx) {
       await getMessengerProfile();
       break;
     case 'set':
-      await setMessengerProfile();
+      await setMessengerProfile(ctx);
       break;
     case 'delete':
     case 'del':
