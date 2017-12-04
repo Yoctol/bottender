ded1d1cdff5699bca9e35313290fa85ec39fc5c1
kpman <s92f002@hotmail.com>
2017-12-04 17:40:35 +0800

    Better log info

--- src/cli/shared/__tests__/schema.spec.js
+++ src/cli/shared/__tests__/schema.spec.js
@@ -26,7 +26,17 @@ it('should valid deep nested config', () => {
 
 it('should check too many messenger menu first layer items', () => {
   const validateResult = Joi.validate(tooManyMessengerFirstLayer, schema);
+
   expect(validateResult.error).not.toBeNull();
+
+  const { message, type } = validateResult.error.details[0];
+  const errorPath = validateResult.error.details[0].path.join('.');
+
+  expect(message).toBe(
+    '"call_to_actions" must contain less than or equal to 3 items'
+  );
+  expect(type).toBe('array.max');
+  expect(errorPath).toBe('messenger.profile.persistent_menu.0.call_to_actions');
   expect(validateResult.error.message).toBe(
     'child "messenger" fails because [child "profile" fails because [child "persistent_menu" fails because ["persistent_menu" at position 0 fails because [child "call_to_actions" fails because ["call_to_actions" must contain less than or equal to 3 items]]]]]'
   );
@@ -34,7 +44,19 @@ it('should check too many messenger menu first layer items', () => {
 
 it('should check too many messenger menu second layer items', () => {
   const validateResult = Joi.validate(tooManyMessengerSecondLayer, schema);
+
   expect(validateResult.error).not.toBeNull();
+
+  const { message, type } = validateResult.error.details[0];
+  const errorPath = validateResult.error.details[0].path.join('.');
+
+  expect(message).toBe(
+    '"call_to_actions" must contain less than or equal to 5 items'
+  );
+  expect(type).toBe('array.max');
+  expect(errorPath).toBe(
+    'messenger.profile.persistent_menu.0.call_to_actions.0.call_to_actions'
+  );
   expect(validateResult.error.message).toBe(
     'child "messenger" fails because [child "profile" fails because [child "persistent_menu" fails because ["persistent_menu" at position 0 fails because [child "call_to_actions" fails because ["call_to_actions" at position 0 fails because [child "call_to_actions" fails because ["call_to_actions" must contain less than or equal to 5 items]]]]]]]'
   );
@@ -42,7 +64,19 @@ it('should check too many messenger menu second layer items', () => {
 
 it('should check too many messenger menu third layer items', () => {
   const validateResult = Joi.validate(tooManyMessengerThirdLayer, schema);
+
   expect(validateResult.error).not.toBeNull();
+
+  const { message, type } = validateResult.error.details[0];
+  const errorPath = validateResult.error.details[0].path.join('.');
+
+  expect(message).toBe(
+    '"call_to_actions" must contain less than or equal to 5 items'
+  );
+  expect(type).toBe('array.max');
+  expect(errorPath).toBe(
+    'messenger.profile.persistent_menu.0.call_to_actions.0.call_to_actions.1.call_to_actions'
+  );
   expect(validateResult.error.message).toBe(
     'child "messenger" fails because [child "profile" fails because [child "persistent_menu" fails because ["persistent_menu" at position 0 fails because [child "call_to_actions" fails because ["call_to_actions" at position 0 fails because [child "call_to_actions" fails because ["call_to_actions" at position 1 fails because [child "call_to_actions" fails because ["call_to_actions" must contain less than or equal to 5 items]]]]]]]]]'
   );
@@ -50,7 +84,17 @@ it('should check too many messenger menu third layer items', () => {
 
 it('should check too many LINE rich menus', () => {
   const validateResult = Joi.validate(tooManyLineRichMenusConfig, schema);
+
   expect(validateResult.error).not.toBeNull();
+
+  const { message, type } = validateResult.error.details[0];
+  const errorPath = validateResult.error.details[0].path.join('.');
+
+  expect(message).toBe(
+    '"richMenus" must contain less than or equal to 10 items'
+  );
+  expect(type).toBe('array.max');
+  expect(errorPath).toBe('line.richMenus');
   expect(validateResult.error.message).toBe(
     'child "line" fails because [child "richMenus" fails because ["richMenus" must contain less than or equal to 10 items]]'
   );
