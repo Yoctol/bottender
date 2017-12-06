2d7770a4bf860f8a7dae15b922bc8c46fd471910
KoChing Liang <yagamigin@gmail.com>
2017-12-06 15:39:47 +0800

    with guard statement and destructuring

--- examples/messenger-built-in-nlp/index.js
+++ examples/messenger-built-in-nlp/index.js
@@ -8,16 +8,22 @@ const bot = new MessengerBot({
 
 bot.onEvent(async context => {
   console.log(context.event.message.nlp.entities);
-  const entities = context.event.message.nlp.entities;
-  if (entities.datetime && entities.datetime[0].type === 'value') {
-    const datetime = entities.datetime[0].value;
-    await context.sendText(`Do you mean ${datetime} ? :)`);
-  } else if (entities.datetime && entities.datetime[0].type === 'interval') {
-    const from = entities.datetime[0].from.value;
-    const to = entities.datetime[0].to.value;
-    await context.sendText(`Do you mean from ${from} to ${to} ? :)`);
-  } else {
+  const { datetime } = context.event.message.nlp.entities;
+
+  if (!datetime) {
     await context.sendText('Not a date or time');
+    return;
+  }
+
+  const dt = datetime[0];
+
+  if (dt.type === 'value') {
+    await context.sendText(`Did you mean ${dt.value} ? :)`);
+  } else if (dt.type === 'interval') {
+    const { from, to } = dt;
+    await context.sendText(
+      `Did you mean from ${from.value} to ${to.value} ? :)`
+    );
   }
 });
 
