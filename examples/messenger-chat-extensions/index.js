b68036ea4f4aeadf30d5d7cf361403598be49ad7
吳東曄 Wu, Dung-Ie <tw0517tw@gmail.com>
2017-12-07 17:11:39 +0900

    add messenger chat extensions example (#92)
    * add messenger chat extensions example
    
    * remove jQuery dependency

---
+++ examples/messenger-chat-extensions/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const { MessengerBot } = require('bottender');
+const { createServer } = require('bottender/express');
+
+const config = require('./bottender.config');
+
+const bot = new MessengerBot(config.messenger);
+
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+const server = createServer(bot, config.messenger);
+
+// https://developers.facebook.com/docs/messenger-platform/webview/extensions#webview_on_web
+// To support Messenger Extensions SDK on desktop Messenger client, server must return X-Frame-Options header.
+const ALLOWED_BY = new Set([
+  'https://www.messenger.com/',
+  'https://www.facebook.com/',
+]);
+
+// Serve Chat Extensions webview
+server.get('/index.html', (req, res) => {
+  // https://github.com/helmetjs/frameguard#allowing-from-multiple-origins
+  const domain = String(req.query.domain);
+  if (ALLOWED_BY.has(domain)) {
+    res.setHeader('X-Frame-Options', `ALLOW-FROM ${domain}`);
+  }
+  res.sendFile(path.resolve(`${__dirname}/index.html`));
+});
+
+server.listen(5000, () => {
+  console.log('server is running on 5000 port...');
+});
