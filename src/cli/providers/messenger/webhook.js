f74d05bb3ae0421454e97760079d129129e0bc4c
xxhomey19 <xxhomey19@gmail.com>
2018-01-23 17:58:02 +0800

    Add set Messenger webhook help

--- src/cli/providers/messenger/webhook.js
+++ src/cli/providers/messenger/webhook.js
@@ -1,12 +1,36 @@
 import { MessengerClient } from 'messaging-api-messenger';
 import invariant from 'invariant';
 import Confirm from 'prompt-confirm';
+import chalk from 'chalk';
 
 import getWebhookFromNgrok from '../../shared/getWebhookFromNgrok';
 import getConfig from '../../shared/getConfig';
 import { print, error, bold, warn } from '../../shared/log';
 
-import help from './help';
+const help = () => {
+  console.log(`
+    bottender messenger webhook <command> [option]
+
+    ${chalk.dim('Commands:')}
+
+      set                   Set Messenger webhook.
+
+    ${chalk.dim('Options:')}
+      -w                    Webhook callback URL
+      -v                    Verify token
+      --ngrok-port          Ngrok port(default: 4040)
+
+    ${chalk.dim('Examples:')}
+
+    ${chalk.dim('-')} Set Messenger webhook url
+
+      ${chalk.cyan('$ bottender messenger webhook set -w http://example.com')}
+
+    ${chalk.dim('-')} Set verify token
+
+      ${chalk.cyan('$ bottender messenger webhook set -v abc123')}
+  `);
+};
 
 export async function setWebhook(webhook, verifyToken, ngrokPort = '4040') {
   try {
@@ -97,13 +121,19 @@ export async function setWebhook(webhook, verifyToken, ngrokPort = '4040') {
 
 export default async function main(ctx) {
   const subcommand = ctx.argv._[2];
-  if (subcommand === 'set') {
-    const webhook = ctx.argv.w;
-    const verifyToken = ctx.argv.v;
-    const ngrokPort = ctx.argv['ngrok-port'];
-    await setWebhook(webhook, verifyToken, ngrokPort);
-  } else {
-    error(`Please specify a valid subcommand: set`);
-    help();
+  switch (subcommand) {
+    case 'set': {
+      const webhook = ctx.argv.w;
+      const verifyToken = ctx.argv.v;
+      const ngrokPort = ctx.argv['ngrok-port'];
+      await setWebhook(webhook, verifyToken, ngrokPort);
+      break;
+    }
+    case 'help':
+      help();
+      break;
+    default:
+      error(`Please specify a valid subcommand: set`);
+      help();
   }
 }
