932e8548ff0411432e4429a35b7a4f2f6c17a34c
tigercosmos <phy.tiger@gmail.com>
2018-06-06 23:31:11 +0800

    refactor

--- examples/cross-platform-with-console/index.js
+++ examples/cross-platform-with-console/index.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const {
   ConsoleBot,
   MessengerBot,
@@ -8,43 +6,47 @@ const {
   SlackBot,
   ViberBot,
 } = require('bottender');
-const { registerRoutes } = require('bottender/express');
 
 const handler = require('./handler');
 const config = require('./bottender.config');
 
-const server = express();
+if (process.env.USE_CONSOLE === 'true') {
+  const bot = new ConsoleBot().onEvent(handler);
+  bot.createRuntime();
+} else {
+  const express = require('express');
+  const bodyParser = require('body-parser');
 
-server.use(
-  bodyParser.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
-    },
-  })
-);
+  const { registerRoutes } = require('bottender/express');
 
-const bots = {
-  console: new ConsoleBot().onEvent(handler),
-  messenger: new MessengerBot(config.messenger).onEvent(handler),
-  line: new LineBot(config.line).onEvent(handler),
-  slack: new SlackBot(config.slack).onEvent(handler),
-  telegram: new TelegramBot(config.telegram).onEvent(handler),
-  viber: new ViberBot(config.viber).onEvent(handler),
-};
+  const server = express();
 
-if (process.argv[2] === 'console') {
-  bots.console.createRuntime();
+  server.use(
+    bodyParser.json({
+      verify: (req, res, buf) => {
+        req.rawBody = buf.toString();
+      },
+    })
+  );
+
+  const bots = {
+    messenger: new MessengerBot(config.messenger).onEvent(handler),
+    line: new LineBot(config.line).onEvent(handler),
+    slack: new SlackBot(config.slack).onEvent(handler),
+    telegram: new TelegramBot(config.telegram).onEvent(handler),
+    viber: new ViberBot(config.viber).onEvent(handler),
+  };
+
+  registerRoutes(server, bots.messenger, {
+    path: '/messenger',
+    verifyToken: config.messenger.verifyToken,
+  });
+  registerRoutes(server, bots.line, { path: '/line' });
+  registerRoutes(server, bots.slack, { path: '/slack' });
+  registerRoutes(server, bots.telegram, { path: '/telegram' });
+  registerRoutes(server, bots.viber, { path: '/viber' });
+
+  server.listen(5000, () => {
+    console.log('server is listening on 5000 port...');
+  });
 }
-
-registerRoutes(server, bots.messenger, {
-  path: '/messenger',
-  verifyToken: config.messenger.verifyToken,
-});
-registerRoutes(server, bots.line, { path: '/line' });
-registerRoutes(server, bots.slack, { path: '/slack' });
-registerRoutes(server, bots.telegram, { path: '/telegram' });
-registerRoutes(server, bots.viber, { path: '/viber' });
-
-server.listen(5000, () => {
-  console.log('server is listening on 5000 port...');
-});
