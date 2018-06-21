de9a5efeb25232a68536220ea802c82c49876134
chentsulin <chentsulin@gmail.com>
2018-06-21 19:37:02 +0800

    update dependencies

--- src/cli/shared/getWebhookFromNgrok.js
+++ src/cli/shared/getWebhookFromNgrok.js
@@ -6,6 +6,7 @@ export default async function getWebhookFromNgrok(ngrokPort) {
     baseURL: `http://localhost:${ngrokPort}`,
   });
 
+  // TODO: use ngrok Client API? - https://ngrok.com/docs#client-api
   const res = await ngrokAxios.get('/api/tunnels');
   return get(res, 'data.tunnels[1].public_url'); // tunnels[1] return `https` protocol
 }
