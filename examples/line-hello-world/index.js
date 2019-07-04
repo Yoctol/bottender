75fe444c13b51f8ab6bbdcedd003858c5127bfa2
handi21048 <52539533+handi21048@users.noreply.github.com>
2019-07-04 23:58:19 +0800

    Update index.js

--- examples/line-hello-world/index.js
+++ examples/line-hello-world/index.js
@@ -2,8 +2,8 @@ const { LineBot } = require('bottender');
 const { createServer } = require('bottender/express');
 
 const bot = new LineBot({
-  channelSecret: '__FILL_YOUR_SECRET_HERE__',
-  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+  channelSecret: 'd941279e4ebe2b25745839fe91ab7240',
+  accessToken: 'vklqL09ScF2CZyoZTjFuP7DU45nOFlu3JDuHZ69/rAbYQakN9mMDB6qIKi86tUnsUTbZmNW+wwFMQXy56tDQiLWMvddHRf7BD5IMyNOrD4DYCxljZZlLzsWMZX5KcJC++2f1UPoDmBXCdH2AD5+AQgdB04t89/1O/w1cDnyilFU=',
 });
 
 bot.onEvent(async context => {
