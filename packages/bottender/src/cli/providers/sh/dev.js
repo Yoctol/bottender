b2d66d57af95fe01c2c3cb6431a92000e1c03fff
chentsulin <chentsulin@gmail.com>
2019-09-05 15:56:29 +0800

    implement bottender start/dev cli

---
+++ packages/bottender/src/cli/providers/sh/dev.js
@@ -0,0 +1,54 @@
+import ngrok from 'ngrok';
+import nodemon from 'nodemon';
+
+import getBottenderConfig from '../../shared/getBottenderConfig';
+
+import getSubArgs from './utils/getSubArgs';
+
+const dev = async ctx => {
+  const argv = getSubArgs(ctx.argv, {
+    '--console': Boolean,
+    '--port': Number,
+
+    // Aliases
+    '-c': '--console',
+    '-p': '--port',
+  });
+
+  const isConsole = argv['--console'];
+  const port = argv['--port'] || 5000;
+
+  const config = getBottenderConfig();
+
+  const { channels = {} } = config;
+
+  // watch
+  nodemon({
+    script: 'node ./start.js',
+    ext: 'js json',
+  })
+    .on('start', () => {
+      console.log('App has started');
+    })
+    .on('quit', () => {
+      console.log('App has quit');
+      process.exit();
+    })
+    .on('restart', files => {
+      console.log('App restarted due to: ', files);
+    });
+
+  if (!isConsole) {
+    const url = await ngrok.connect(port);
+
+    Object.entries(channels)
+      .filter(([, { enabled }]) => enabled)
+      .forEach(([channel, { path: webhookPath }]) => {
+        const routePath = webhookPath || `/webhooks/${channel}`;
+
+        console.log(`${channel} webhook url: ${url}${routePath}`);
+      });
+  }
+};
+
+export default dev;
