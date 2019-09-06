dda49ceb840ae8a18c1afafeee8f0e094f2d6251
chentsulin <chentsulin@gmail.com>
2019-09-06 14:33:32 +0800

    feat: pass object as first argument (FileSessionStore, MemorySessionStore)

--- packages/bottender/src/session/MemorySessionStore.js
+++ packages/bottender/src/session/MemorySessionStore.js
@@ -7,10 +7,29 @@ import { type SessionStore } from './SessionStore';
 
 const MINUTES_IN_ONE_YEAR = 365 * 24 * 60;
 
+type MemoryOption =
+  | number
+  | {
+      maxSize?: ?number,
+    };
+
+function getＭaxSize(arg: MemoryOption): ?number {
+  if (typeof arg === 'number') {
+    return arg;
+  }
+
+  if (arg && typeof arg === 'object') {
+    return arg.maxSize;
+  }
+}
+
 export default class MemorySessionStore extends CacheBasedSessionStore
   implements SessionStore {
-  constructor(max: number, expiresIn: number) {
-    const cache = new MemoryCacheStore(max);
+  constructor(arg: MemoryOption, expiresIn: number) {
+    const maxSize = getＭaxSize(arg);
+
+    const cache = new MemoryCacheStore(maxSize);
+
     super(cache, expiresIn || MINUTES_IN_ONE_YEAR);
   }
 }
