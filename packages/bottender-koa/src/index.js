ecd108cde80b0f75b42136534383b39951efe9a4
chentsulin <chentsulin@gmail.com>
2019-09-06 15:56:42 +0800

    remove unused flow annotation

--- packages/bottender-koa/src/index.js
+++ packages/bottender-koa/src/index.js
@@ -1,5 +1,3 @@
-/* @flow */
-
 export { default as createServer } from './createServer';
 export { default as createMiddleware } from './createMiddleware';
 export { default as registerRoutes } from './registerRoutes';
