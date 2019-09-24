ec38a3bce3830acfdb3bf9ed2aa7a2ac51f3b7f4
chentsulin <chentsulin@gmail.com>
2019-09-25 00:36:01 +0800

    removed: get-started, greeting, persistent-menu, whitelisted-domains. Use profile instead

--- packages/bottender/src/cli/providers/messenger/__tests__/index.spec.ts
+++ packages/bottender/src/cli/providers/messenger/__tests__/index.spec.ts
@@ -1,32 +1,13 @@
 import messenger from '..';
 
-jest.mock('../get-started');
-jest.mock('../greeting');
-jest.mock('../persistent-menu');
 jest.mock('../profile');
 jest.mock('../webhook');
-jest.mock('../whitelisted-domains');
 jest.mock('../help');
 jest.mock('../persona');
 
 describe('messenger cli', () => {
   it('should exist', () => {
     expect(messenger).toBeDefined();
-  });
-
-  it('should return getStarted module', () => {
-    const getStarted = require('../get-started').default;
-    expect(messenger.getStarted).toEqual(getStarted);
-  });
-
-  it('should return greeting module', () => {
-    const greeting = require('../greeting').default;
-    expect(messenger.greeting).toEqual(greeting);
-  });
-
-  it('should return persistentMenu module', () => {
-    const persistentMenu = require('../persistent-menu').default;
-    expect(messenger.persistentMenu).toEqual(persistentMenu);
   });
 
   it('should return profile module', () => {
@@ -39,24 +20,9 @@ describe('messenger cli', () => {
     expect(messenger.webhook).toEqual(webhook);
   });
 
-  it('should return whitelistedDomains module', () => {
-    const whitelistedDomains = require('../whitelisted-domains').default;
-    expect(messenger.whitelistedDomains).toEqual(whitelistedDomains);
-  });
-
   it('should return help module', () => {
     const help = require('../help').default;
     expect(messenger.help).toEqual(help);
-  });
-
-  it('should return persistentMenu module when gettig menu', () => {
-    const persistentMenu = require('../persistent-menu').default;
-    expect(messenger.menu).toEqual(persistentMenu);
-  });
-
-  it('should return whitelistedDomains module when gettig domains', () => {
-    const whitelistedDomains = require('../whitelisted-domains').default;
-    expect(messenger.domains).toEqual(whitelistedDomains);
   });
 
   it('should return persona module', () => {
--- packages/bottender/src/cli/providers/messenger/help.ts
+++ packages/bottender/src/cli/providers/messenger/help.ts
@@ -6,15 +6,9 @@ const help = (): void => {
 
     ${chalk.dim('Commands:')}
 
-      get-started             <action>    Manage get-started
-      greeting                <action>    Manage greeting text
-      menu, persistent-menu   <action>    Manage persistent-menu
       profile                 <action>    Manage Messenger profile
       webhook                 <action>    Manage webhook
-      domains,
-      whitelisted-domains     <action>    Manage whitelisted-domains
       persona                 <action>    Manage persona
-      attachment              <action>    Manage attachment
 
     ${chalk.dim('Actions:')}
 
@@ -27,14 +21,6 @@ const help = (): void => {
     ${chalk.dim('-')} Set the messenger profile
 
       ${chalk.cyan('$ bottender messenger profile set')}
-
-    ${chalk.dim('-')} Delete the persistent-menu
-
-      ${chalk.cyan('$ bottender messenger persistent-menu del')}
-
-    ${chalk.dim('-')} Show attachment command usage
-
-      ${chalk.cyan('$ bottender messenger attachment help')}
   `);
 };
 
