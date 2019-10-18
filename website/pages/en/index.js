0cfb0d7eed202e5e3365ce8561b069822cf65153
chentsulin <chentsulin@gmail.com>
2019-10-18 17:30:34 +0800

    use new wording

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -73,17 +73,17 @@ class Index extends Component {
               {[
                 {
                   content:
-                    'Build encapsulated handlers that manage your business logic, then compose them to make complex Bots. Handlers are just JavaScript  functions which can achieve whatever you want.',
+                    'Bottender takes care of the complexity of conversational UIs for you. Design actions for each event and state in your application, and Bottender will run accordingly.\n\nThis approach makes your code more predictable and easier to debug.',
                   image: `${baseUrl}img/element_flexible.svg`,
                   imageAlign: 'left',
-                  title: 'Flexible',
+                  title: 'Declarative',
                 },
                 {
                   content:
-                    'Bottender is written with ES6/ES7 up syntax and it fully supports async  await  function. We can deal with complex async logic without losing  the  readability and maintainability of code.',
+                    'Bottender lets you create apps on every channel and never compromise on your users’ experience.\n\nYou can apply progressive enhancement or graceful degradation strategy on your building blocks.',
                   image: `${baseUrl}img/element_morden.svg`,
                   imageAlign: 'left',
-                  title: 'Modern',
+                  title: 'Native User Experience',
                 },
               ]}
             </Block>
@@ -92,17 +92,17 @@ class Index extends Component {
               {[
                 {
                   content:
-                    'There is a set of interface that defines the core modules provided by Bottender. You can use your own implementation of session store, your HTTP server framework or even connect your bot to any platform.',
+                    'With Bottender, you only need a few configurations to make your bot work with channels, automatic server listening, webhook setup, signature verification and so much more.',
                   image: `${baseUrl}img/element_modular.svg`,
                   imageAlign: 'left',
-                  title: 'Modular',
+                  title: 'Easy Setup',
                 },
                 {
                   content:
-                    'Handle multiple platforms with consistent development experience. You can  develop bots for different platforms including Messenger, LINE, Slack  and Telegram comfortably, with a similar mindset.',
+                    'There are thousands of bots are powered by Bottender.\n\nIt has been optimized for real world use cases, automatic batching request and dozens of other compelling features.',
                   image: `${baseUrl}img/element_anywhere.svg`,
                   imageAlign: 'left',
-                  title: 'Learn Once, Write Anywhere',
+                  title: 'Ready for Production',
                 },
               ]}
             </Block>
@@ -111,29 +111,44 @@ class Index extends Component {
           <div className="triangle" />
 
           <div className="demo">
-            <h2>Developers First</h2>
             <div className="demo-content">
               <div className="code-block">
                 <MarkdownBlock>
                   {`
-~~~bash
-❯ create-bottender-app
-? What's your project name? awesome-bot
-? What platform of bot do you want to create? messenger
-? Where do you want to store session? (Use arrow keys)
-❯ memory
-  file
-  redis
-  mongo
+~~~javascript
+const { router, text } = require('bottender/router');
+
+function SayHi(context) {
+  await context.sendText('Hi!');
+}
+
+function Unknown(context) {
+  await context.sendText('Sorry, I don’t know what you say.');
+}
+
+module.export = function App(context) {
+  return router([
+    text('hi', SayHi),
+    text('*', Unknown),
+  ]);
+};
 ~~~
               `}
                 </MarkdownBlock>
               </div>
               <div className="description">
-                <h3>Initialize</h3>
+                <h3>Intuitive APIs</h3>
                 <p>
-                  After installed, we can start to create a new bot with
-                  bottender init command
+                  Bottender has some functional and declarative approaches can
+                  help you define your conversations. <br />
+                  <br />
+                  For most applications, you will begin by defining routes that
+                  you may familiar with when developing a web application.{' '}
+                  <br />
+                  <br />
+                  Besides router, you can describe the side effects with
+                  function actions. It’s the most important building part in the
+                  Bottender applications.
                 </p>
               </div>
             </div>
