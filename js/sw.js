e2544236c7aa5f00d24175f9f08c9a683b640a06
chentsulin <chentsulin@gmail.com>
2019-11-04 17:57:58 +0800

    Deploy website
    Deploy website version based on e632fa093a259f5a1e23d0bb3f6cd44f4170acea

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,5 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then(registrations => {
     registrations.forEach(registration => registration.unregister());
-    location.reload();
   });
 }
