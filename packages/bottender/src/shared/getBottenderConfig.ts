6e6144fde75fbb2efc71bfcb3a2637426ad63d67
chentsulin <chentsulin@gmail.com>
2019-11-12 16:51:28 +0800

    fix(getBottenderConfig): use {} as default bottender.config.js

--- packages/bottender/src/shared/getBottenderConfig.ts
+++ packages/bottender/src/shared/getBottenderConfig.ts
@@ -10,8 +10,12 @@ dotenv.config();
  * By default, it will try to require the module from `<root>/bottender.config.js`.
  */
 const getBottenderConfig = (): BottenderConfig | never => {
-  // eslint-disable-next-line import/no-dynamic-require, @typescript-eslint/no-var-requires
-  return require(path.resolve('bottender.config.js'));
+  try {
+    // eslint-disable-next-line import/no-dynamic-require, @typescript-eslint/no-var-requires
+    return require(path.resolve('bottender.config.js'));
+  } catch (err) {
+    return {};
+  }
 };
 
 export default getBottenderConfig;
