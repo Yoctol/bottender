bea3a3a21964656ce6c62e5f400d4b856f0db8e5
chentsulin <chentsulin@gmail.com>
2019-11-28 13:41:23 +0800

    docs: api reference

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: 'getting-started', label: 'Docs' },
-    // { doc: 'api-context', label: 'API' },
+    { doc: 'api-context', label: 'API' },
     { blog: true, label: 'Blog' },
     {
       href: 'https://github.com/Yoctol/bottender/tree/master/examples',
