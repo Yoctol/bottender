053db1b07ad184b013055e6503b6cf22725ffa24
etrex <et284vu065k3@gmail.com>
2020-02-04 15:29:55 +0800

    feat: example for line liff v1 (#640)

---
+++ examples/line-liff-v1/server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const { bottender } = require('bottender');
+
+const app = bottender({
+  dev: process.env.NODE_ENV !== 'production',
+});
+
+const port = Number(process.env.PORT) || 5000;
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(
+    bodyParser.json({
+      verify: (req, _, buf) => {
+        req.rawBody = buf.toString();
+      },
+    })
+  );
+
+  server.get('/liff', (req, res) => {
+    const filename = path.join(`${__dirname}/liff.html`);
+    res.sendFile(filename);
+  });
+
+  // delegate other requests to bottender
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
