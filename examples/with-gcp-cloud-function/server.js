7b9cdc26f4b030697d327e7f1375e4edd5827486
Cheng-Wei Hu <32091985+HcwXd@users.noreply.github.com>
2020-02-11 10:54:39 +0800

    examples: add gcp cloud function example (#649)

---
+++ examples/with-gcp-cloud-function/server.js
@@ -0,0 +1,36 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const { bottender } = require('bottender');
+
+const app = bottender({
+  dev: process.env.NODE_ENV !== 'production',
+});
+
+const port = Number(process.env.PORT) || 5000;
+
+const handle = app.getRequestHandler();
+
+const server = express();
+server.use(
+  bodyParser.json({
+    verify: (req, _, buf) => {
+      req.rawBody = buf.toString();
+    },
+  })
+);
+
+server.all('*', (req, res) => {
+  app.prepare().then(() => {
+    return handle(req, res);
+  });
+});
+
+server.listen(port, err => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log(`> Ready on http://localhost:${port}`);
+});
+
+module.exports = { server };
