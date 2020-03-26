e77521036b7f35dfc13aea415c246406ffacbd9c
chentsulin <chentsulin@gmail.com>
2020-03-26 15:01:15 +0800

    fix(Server): fix responding with application/json when using custom servers

--- packages/bottender/src/server/Server.ts
+++ packages/bottender/src/server/Server.ts
@@ -149,7 +149,16 @@ class Server {
               res.setHeader(key, value as string);
             });
             res.statusCode = response.status || 200;
-            res.end(response.body || '');
+            if (
+              response.body &&
+              typeof response.body === 'object' &&
+              !Buffer.isBuffer(response.body)
+            ) {
+              res.setHeader('Content-Type', 'application/json; charset=utf-8');
+              res.end(JSON.stringify(response.body));
+            } else {
+              res.end(response.body || '');
+            }
           } else {
             res.statusCode = 200;
             res.end('');
