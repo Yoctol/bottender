b4c07174bfcadeb42d4b39d4eb63bfe91ab66e03
chentsulin <chentsulin@gmail.com>
2020-05-18 10:32:35 +0800

    example(messenger-batch-multi-pages): remove page id env in bottender.config.js

--- examples/messenger-batch-multi-pages/bottender.config.js
+++ examples/messenger-batch-multi-pages/bottender.config.js
@@ -27,7 +27,6 @@ module.exports = {
     messenger: {
       enabled: true,
       path: '/webhooks/messenger',
-      pageId: process.env.MESSENGER_PAGE_ID,
       accessToken: process.env.MESSENGER_ACCESS_TOKEN,
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
