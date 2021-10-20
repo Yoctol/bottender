b71a97a4e576da38506ff422e156821240b73f4a
tw0517tw <1003146+tw0517tw@users.noreply.github.com>
2021-10-21 07:06:30 +0800

    docs: add users and versions dummy file

---
+++ website/src/pages/users.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import Layout from '@theme/Layout';
+
+const Users = () => {
+  return <Layout>Users</Layout>;
+};
+
+export default Users;
---
+++ website/src/pages/versions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import Layout from '@theme/Layout';
+
+const Versions = () => {
+  return <Layout>Versions</Layout>;
+};
+
+export default Versions;
