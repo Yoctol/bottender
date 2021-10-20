da1c3d7e21c4ecb1edf2ef9d38e8788ebde27482
tw0517tw <1003146+tw0517tw@users.noreply.github.com>
2021-10-21 04:27:43 +0800

    docs: fix gitignore for generated files

--- v2-website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\tw0517tw\\yoctol\\bottender\\v2-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\tw0517tw\\yoctol\\bottender\\v2-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\tw0517tw\\yoctol\\bottender\\v2-website\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css"),
-  require("../src/css/customTheme.css"),
-];
